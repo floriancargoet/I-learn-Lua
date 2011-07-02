be0cecb400abf30b19d344b69aaecef20b8c9143
Florian Cargoët <florian.cargoet+github@centrale-marseille.fr>
2011-07-02 20:26:30 +0200

    added namespace_merge

--- namespace.lua
+++ namespace.lua
@@ -38,3 +38,42 @@ namespace('a.b.c'){
 print(a.b.c.w) --> 3
 print(a.b.y.z) --> 2
 print(a.b.c.d) --> nil, d has been replaced with { w = 3 }
+
+
+-- with merge
+function namespace_merge(str)
+    local ns = _G   -- starts with the global table
+    local last_part -- will contain the name of the last table
+    
+    -- I can't find a split-like function, inspired by http://www.lua.org/pil/14.1.html
+    for part, dot in string.gfind(str, "([%w_]+)(.?)") do
+        if dot == '.' then
+            ns[part] = ns[part] or {}
+            ns = ns[part]
+        else
+            last_part = part
+        end
+    end
+    return function(t)
+        if ns[last_part] then
+            local ns = ns[last_part]
+            for k, v in pairs(t) do
+                ns[k] = v
+            end
+        else
+            ns[last_part] = t
+        end
+    end
+end
+
+--nice with merges now
+namespace_merge('a.b.c'){
+    y = 4
+}
+
+namespace_merge('a.b.y'){
+    z = 5 -- overwrite existing a.b.y.z
+}
+print(a.b.c.y) --> 4
+print(a.b.c.w) --> 3, still there
+print(a.b.y.z) --> 5
