e9cecfc1a7add60c51464ae7671e7f3a495bf538
Florian Cargoët <florian.cargoet+github@centrale-marseille.fr>
2011-07-11 16:45:48 +0200

    added performance tests

--- unpack2.lua
+++ unpack2.lua
@@ -9,4 +9,36 @@ unpack2 = function(t1, t2, i)
     end
 end
 
-print( unpack2({1, 2, 3}, {4, 5, 6}) )
+unpack3 = function(t1, t2)
+    local t = {unpack(t1)}
+    for i, v in ipairs(t2) do
+        table.insert(t, t2[i])
+    end
+    return unpack(t)
+end
+
+--[[
+
+local t1 = {1, 2, 3}
+local t2 = {4, 5, 6}
+
+print( unpack2(t1, t2) )
+print( unpack3(t1, t2) )
+
+local time = os.clock()
+
+for i = 1, 1000000 do
+    unpack2(t1, t2)
+end
+
+print (os.clock() - time);
+time = os.clock()
+
+for i = 1, 1000000 do
+    unpack3(t1, t2)
+end
+
+
+print (os.clock() - time);
+
+--]]
