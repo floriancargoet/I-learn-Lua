6099793390dba71e6d38ce5816aaa0b4e1bd073b
Florian Cargoët <florian.cargoet+github@centrale-marseille.fr>
2011-07-16 00:17:03 +0200

    added method picking in Mixin

--- serious/mixin.lua
+++ serious/mixin.lua
@@ -1,14 +1,16 @@
 -- a Mixin class
 -- Usage:
---  * create a new mixin with methods
+--  * create a new mixin with properties
 --     Closable = Mixin.create()
---     function Closable:close() print('close') end
+--     function Closable:close()
+--         print('close')
+--     end
 --  * apply the mixin to a "class" or an instance
 --     Closable.mixin(Window)
 --     w = Window:create()
 --     w:close() --> prints 'close'
 --
--- mixin() fails if method names conflict
+-- mixin() fails if property names conflict
 
 Mixin = {}
 
@@ -20,13 +22,48 @@ function Mixin:create(mixin) -- type: table
     return mixin
 end
 
-function Mixin:mixin(class) -- type: table
-    for name, method in pairs(self) do
+function Mixin:mixin(class, ...) -- type: table, string...
+    local source
+    -- gather properties to be copied
+    if arg[1] then
+        -- only mixin certain methods/properties
+        source = {}
+        for i, name in ipairs(arg) do
+            source[name] = self[name]
+        end
+    else
+        -- copy all
+        source = self
+    end
+
+    -- copy methods/properties
+    for name, prop in pairs(source) do
         if class[name] then
-            error("Method "..name.." already exists!", 2)
+            error("Property "..name.." already exists!", 2)
         else
-            class[name] = method
+            class[name] = prop
         end
     end
     return class
 end
+
+--tests
+---[[
+local Closable = Mixin:create({
+    close = function()
+         print('close')
+     end,
+     open = function()
+         print('open')
+     end
+})
+
+local a = Closable:mixin({})
+a:open()
+a:close()
+
+local b = Closable:mixin({}, 'open')
+b:open()
+print(b.close) -- not here
+
+--]]
